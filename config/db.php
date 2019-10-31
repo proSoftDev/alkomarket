816ac62feda6986ac22d707f9cd303e45868c723
Nurzat <nurzat.kaz@gmail.com>
2019-10-31 12:55:59 +0600

    db changes

--- config/db.php
+++ config/db.php
@@ -8,11 +8,11 @@ return [
     'charset' => 'utf8',
 
 
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=srv-pleskdb47.ps.kz:3306;dbname=chatbots_gradus',
-    'username' => 'chatb_gradus',
-    'password' => '1q2w3e4r',
-    'charset' => 'utf8',
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=srv-pleskdb47.ps.kz:3306;dbname=chatbots_gradus',
+//    'username' => 'chatb_gradus',
+//    'password' => '1q2w3e4r',
+//    'charset' => 'utf8',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
